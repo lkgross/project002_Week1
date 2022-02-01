630d73ffca75faab3aefcc033e445bc08e1db970
Gross <L1GROSS@bridgew.edu>
2022-02-01 14:20:08 -0500

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Main: Class to serve as a container to hold the program
+ */
+public class HelloWorld {
+    /**
+     * main: The main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        // write your code here
+        /*
+            If you use print, you don't get a newline character at the end.
+         */
+        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+
+        System.out.print("Hello, World!\n");
+        System.out.println("Hello, World!");
+
+        System.out.println("\tHello, World!");
+        System.out.println("Hello, World!");
+    }
+}
